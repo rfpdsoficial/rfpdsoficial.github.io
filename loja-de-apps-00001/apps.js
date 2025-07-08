5b06de10365a9398be68b20b42bd11336a6b3e07
rfpdsoficial <Raphaelpereiradossantos3@gmail.com>
2025-07-08 11:02:00 -0300

    Add files via upload

---
+++ loja-de-apps-00001/apps.js
@@ -0,0 +1,103 @@
+const apps = {
+    "apps": [
+        {
+            "id": 1,
+            "name": "Subway Surfers Mod",
+            "description": "Do nosso site você pode baixar Subway Surfers com dinheiro infinito e todos os recursos desbloqueados. Desenvolvedor: Jogos SYBO, Atualização 2 de julho de 2025, Tamanho (Size), 135 MB, Versão 3.48.5 (Modelo), Requisitos Andróide 4.4+",
+            "category": "Jogos",
+            "price": 0.00,
+            "promo_price": 0.00,
+            "is_featured": false,
+            "is_promo": false,
+            "rating": 4.8,
+            "downloads": "1M+",
+            "image": "https://subway-surfers-4.apkgara.com/uploads/2021/7/subway-surfers-icon.jpg",
+            "download_link": "https://www.mediafire.com/file/glby3ps7y0q1ohh/Subway-Surfers-3.48.4-Mod-APKGara.Com.apk/file"
+        },
+        {
+            "id": 2,
+            "name": "Spotify Premium APK Mod 9.0.60.128 (Desbloqueado)",
+            "description": "Spotify Premium APK Download MediaFire grátis para Android - Atualizado 2025. Crie playlists e distribua todo o seu conhecimento ou emoções musicais para o mundo, Nome: Spotify Premium, Desenvolvedor: Spotify Ltd. Categoria: Música e áudio, Última versão: V9.0.60.128, Atualizado: Jul 7, 2025, MOD: Desbloqueado, Tamanho: 84.59 Mb.",
+            "category": "Musica",
+            "price": 0.00,
+            "promo_price": 0.00,
+            "is_featured": false,
+            "is_promo": false,
+            "rating": 4.5,
+            "downloads": "500K+",
+            "image": "https://spotify-premium.apkgara.com/uploads/2021/7/spotify-premium-icon.jpg",
+            "download_link": "https://www.mediafire.com/file/9cfl9472qw0mya3/Spotify-v9.0.60.128-Mod-APKGara.Com.apk/file"
+        },
+        {
+            "id": 3,
+            "name": "YouTube Premium APK Mod 20.26.55 (Desbloqueado)",
+            "description": "Download YouTube Premium APK Mod mediafıre grátis - Atualizar 2025. Conheça e aproveite todos os benefícios do maior e melhor aplicativo de vídeos do momento, Nome: YouTube Premium, Desenvolvedor: Google LLC, Categoria: Reproduzir e editar vídeos, Última versão: V20.26.55, Atualizado: Jul 7, 2025, MOD: Desbloqueado, Tamanho: 153.32 Mb, instruções de instalação: Instale microG, Instale o YouTube Premium, Faça login na sua conta. link do microg:https://www.mediafire.com/file/43z2k978fmafi38/File-MicroG-Youtube-Premium-APKGara.Com.apk/file",
+            "category": "Social",
+            "price": 0.00,
+            "promo_price": 0.00,
+            "is_featured": false,
+            "is_promo": false,
+            "rating": 5.6,
+            "downloads": "500K+",
+            "image": "https://youtube-premium.apkgara.com/uploads/2021/7/youtube-premium-icon.jpg",
+            "download_link": "https://www.mediafire.com/file/9cfl9472qw0mya3/Spotify-v9.0.60.128-Mod-APKGara.Com.apk/file"
+        },
+        {
+            "id": 4,
+            "name": "GTA San Andreas APK Mod 2.11.245 (Dinheiro infinito)",
+            "description": "GTA San Andreas APK Mod Download mediafıre grátis para Android 2025 – gratuito para Android e inicie sua aventura como CJ para libertar San Andres do tráfico, Dinheiro infinito, Armas e munições ilimitadas God mode, Desenvolvedor:Rockstar Games, Categoria:Ação, Última versão: V2.11.245, Atualizado:Sep 26, 2024, MOD: Dinheiro infinito, Tamanho: 900 MB.",
+            "category": "Jogos",
+            "price": 0.00,
+            "promo_price": 0.00,
+            "is_featured": false,
+            "is_promo": false,
+            "rating": 4.6,
+            "downloads": "0K+",
+            "image": "https://gta-san-andreas.apkgara.com/uploads/2021/7/gta-san-andreas-icon-1.jpg",
+            "download_link": "https://www.mediafire.com/file/3fhyci0agpktvmt/GTA-San-Andreas-2.11.245-Mod-APKGara.Com.apk/file"
+        },
+        {
+            "id": 5,
+            "name": "Minecraft APK 1.21.93.1",
+            "description": "Minecraft APK 2025 permite que você crie, explore e sobreviva em um universo de blocos. Seja construindo obras-primas ou enfrentando perigos, sua aventura te aguarda, Desenvolvedor:Mojang, Categoria:Arcade, Última versão: V1.21.93.1, Atualizado:Jul 4, 2025, MOD: Sem MOD, Tamanho: 894.07 Mb, Destaque: Imortal.",
+            "category": "Jogos",
+            "price": 0.00,
+            "promo_price": 0.00,
+            "is_featured": false,
+            "is_promo": false,
+            "rating": 4.4,
+            "downloads": "0K+",
+            "image": "https://minecraft-2.apkgara.com/uploads/2021/7/minecraft-icon.jpg",
+            "download_link": "https://www.mediafire.com/file/ynap000qcjl4s69/Minecraft-1.21.92.1-Mod-APKGara.Com.apk/file"
+        },
+        {
+            "id": 6,
+            "name": "Tomb of the Mask: Dinero infinito1.21.2 (Mod)",
+            "description": "Tomb of the Mask APK Mediafıre oferece ação emocionante num labirinto de surpresas. Download a versão mais recente para Android em 2025 gratuitamente, Moedas ilimitadas (aumentam quando gastas), Desenvolvedor: Playgendary Limited, Atualização: 6 de julho de 2025, Tamanho (Size) 60 MB, Versão: 1.21.2 (Modelo), Requisitos: Android 5.0+.",
+            "category": "Jogos",
+            "price": 0.00,
+            "promo_price": 0.00,
+            "is_featured": false,
+            "is_promo": false,
+            "rating": 4.6,
+            "downloads": "0K+",
+            "image": "https://apkgara.com/uploads/2024/6/tomb-of-the-mask-thumbnail.jpg",
+            "download_link": "https://www.mediafire.com/file/im6fy6zwnxghvel/tomb%20of%20the%20mask%20mod%20vhttps://www.mediafire.com/file/im6fy6zwnxghvel/tomb%20of%20the%20mask%20mod%20v1.21.2%20-%20espacioapk.com.apk/file%20-%20espacioapk.com.apk/file"
+        },
+        {
+            "id": 7,
+            "name": "Lark Player Premium APK Mod 6.19.6 (Desbloqueado)",
+            "description": "Lark Player Premium APK, um dos melhores aplicativos de música e áudio para Android, permite que você baixe e desfrute de uma nova versão de áudio imersivo em 2025, Pro desbloqueado, Desenvolvedor:Lark Player Studio - Music, MP3 & Video Player, Categoria:Música e áudio, Última versão: V6.19.6, Atualizado:May 8, 2025, MOD: Desbloqueado, Tamanho: 20.65 Mb.",
+            "category": "Musica",
+            "price": 0.00,
+            "promo_price": 0.00,
+            "is_featured": false,
+            "is_promo": false,
+            "rating": 4.7,
+            "downloads": "0K+",
+            "image": "https://apkgara.com/uploads/2024/6/lark-playermusic-player-mp3-thumbnail.jpg",
+            "download_link": "https://www.mediafire.com/file/epybg4aqn2o9f4f/LarkPlayer-6.13.6-Mod-APKGara.Com.apk/file"
+        }
+
+    ]
+};
